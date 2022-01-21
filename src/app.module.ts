d5ec9a28779d1fe2f2efadc9a189c12473b35ca1
Zach Hawkes <zach@ifit.com>
2022-01-21 16:25:07 -0700

    working on prisma integration and todos schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { TodoModule } from './todo/todo.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, TodoModule],
   controllers: [AppController],
   providers: [AppService],
 })
